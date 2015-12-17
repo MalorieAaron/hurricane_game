ff9838f1fba424efd8b4207090388b1618b9721e
Malorie Gomm <maloriehauoligomm@gmail.com>
2015-12-16 21:33:49 -0700

    finished camping store - we don't have limits on how many they can buy.

--- src/view/StoreCampingAislesView.java
+++ src/view/StoreCampingAislesView.java
@@ -12,6 +12,9 @@
 
 package view;
 
+import model.Game;
+import model.Item;
+
 /**
  *
  * @author Maxine
@@ -20,28 +23,85 @@ public class StoreCampingAislesView extends View {
     
     public StoreCampingAislesView() {
         super ("Camping Store Aisles:\n"
-                + "T - Tent\n"
-                + "A - Tarp\n"
-                + "F - Fuel\n"
-                + "C - Camp Stove\n"
+                + "T - Tent: $" + Item.TENT.getItemCost() + " | " + Item.TENT.getSurvivalPoints() + " Survival Points\n"
+                + "A - Tarp: $" + Item.TARP.getItemCost() + " | " + Item.TARP.getSurvivalPoints() + " Survival Points\n"
+                + "F - Fuel: $" + Item.FUEL.getItemCost() + " | " + Item.FUEL.getSurvivalPoints() + " Survival Points\n"
+                + "C - Camp Stove: $" + Item.CAMP_STOVE.getItemCost() + " | " + Item.CAMP_STOVE.getSurvivalPoints() + " Survival Points\n"
                 + "X - Return to Game Menu");
     }
     
     @Override
     public boolean doAction(char input) {
     
+        int currentMoney = Game.getInstance().getPlayer().getMoney();
+        int currentSP = Game.getInstance().getPlayer().getCurrentSurvivalPoints();
+        
         switch(input) {
             case 'T':
-                System.out.println("You selected a tent for purchase.");
+                System.out.println("You bought a tent.");
+                String itemName = Item.TENT.getItemName();
+                int itemCost = Item.TENT.getItemCost();
+                int itemSP = Item.TENT.getSurvivalPoints();
+
+                int newMoney = currentMoney - itemCost;
+
+                Game.getInstance().getPlayer().setMoney(newMoney);
+
+                int newSP = currentSP + itemSP;
+                Game.getInstance().getPlayer().setCurrentSurvivalPoints(newSP);
+
+                displayMoney();
+                displaySP();
                 break;
             case 'A':
-                System.out.println("You selected a tarp for purchase.");
+                 System.out.println("You bought a Tarp.");
+                itemName = Item.TARP.getItemName();
+                itemCost = Item.TARP.getItemCost();
+                itemSP = Item.TARP.getSurvivalPoints();
+
+                newMoney = currentMoney - itemCost;
+
+                Game.getInstance().getPlayer().setMoney(newMoney);
+
+                newSP = currentSP + itemSP;
+                Game.getInstance().getPlayer().setCurrentSurvivalPoints(newSP);
+
+                displayMoney();
+                displaySP();
                 break;
+                
             case 'F':
-                System.out.println("You selected fuel for purchase.");
+                 System.out.println("You bought fuel.");
+                itemName = Item.FUEL.getItemName();
+                itemCost = Item.FUEL.getItemCost();
+                itemSP = Item.FUEL.getSurvivalPoints();
+
+                newMoney = currentMoney - itemCost;
+
+                Game.getInstance().getPlayer().setMoney(newMoney);
+
+                newSP = currentSP + itemSP;
+                Game.getInstance().getPlayer().setCurrentSurvivalPoints(newSP);
+
+                displayMoney();
+                displaySP();
                 break;
             case 'C':
-                System.out.println("You selected a stove for purchase.");
+                System.out.println("You bought a camp stove.");
+                itemName = Item.CAMP_STOVE.getItemName();
+                itemCost = Item.CAMP_STOVE.getItemCost();
+                itemSP = Item.CAMP_STOVE.getSurvivalPoints();
+
+                newMoney = currentMoney - itemCost;
+
+                Game.getInstance().getPlayer().setMoney(newMoney);
+
+                newSP = currentSP + itemSP;
+                Game.getInstance().getPlayer().setCurrentSurvivalPoints(newSP);
+
+                displayMoney();
+                displaySP();
+                break;
             case 'X':
                 return false;
             default:
@@ -51,6 +111,18 @@ public class StoreCampingAislesView extends View {
         return true;
     }    
 
+    
+      private void displayMoney() {
+        
+        System.out.println("You now have $" + Game.getInstance().getPlayer().getMoney() + " in your money account.");
+
+    
+    }
+
+    private void displaySP() {
+        System.out.println("You now have " + Game.getInstance().getPlayer().getCurrentSurvivalPoints() + " Survival Points!");
+    
+    }
     
 
    }
